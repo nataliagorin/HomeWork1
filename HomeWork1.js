9678309ec184f9f1e1c8f30d3b6e8a8163d1ce5e
Natalia Gorin <94384051+nataliagorin@users.noreply.github.com>
2025-05-31 11:10:12 +0300

    spread operator

--- HomeWork1.js
+++ HomeWork1.js
@@ -3,20 +3,21 @@
 
 
 // ------- 1.2. Difference between var, let, and const. -------
-
+console.log("1.2. Difference between var, let, and const: \n");
 // 1.2.1. var 
 // this type of variable is function-scoped or globally scoped, and it can be reinitialized.
 var age = 30; // global scope
 function ageNextYear() {
     var age = 22; // function scope
     age = age + 1; // reinitialization is allowed for var
-    console.log("Next year, I will be " + age + " years old.");
+    console.log("Next year, I will be " + age + " years old. \n");
 }
-ageNextYear(); // Output: Next year, I will be 23 years old.
+ageNextYear(); 
 
 // 1.2.2. let
 // this type of varible was introduced later in ES6, it is block-scoped, and it can be reinitialized and reassigned.
 function countToTen() {
+    console.log("Let's count to ten :)")
     for (let i = 0; i <= 10; i++) {
         console.log(i); 
     }
@@ -32,11 +33,30 @@ function circleArea(radius) {
    // PI = 3.14159; // assignment to const variable is not allowed 
     const PI = 3.14; // const variable, cannot be reassigned
     const area = PI * radius * radius;
-    console.log("The area of the circle is: " + area);
+    console.log("\nThe area of the circle is: " + area + "\n");
 }
 //console.log("PI in global scope: " + PI); 
 
 circleArea(10);
 
-    
 
+// ------- 1.3. Spread operator. -------
+console.log("1.3. Spread operator: \n");
+
+function completeQuote() {
+    const quote = "Whatever we call beautiful, we quiver before it.";
+    const missingPart = "Beauty is terror. ";
+
+    const complete = [...missingPart, ...quote];
+    console.log(complete.join('') + "- Donna Tartt.\n");
+}
+completeQuote();
+
+
+function changePassword(password) {
+    let credentials = { username: "anne", password: "123456" };
+    console.log("Your old credentials are: " + JSON.stringify(credentials));
+    let newCredentials = { ...credentials, password: password };
+    console.log("Your new credentials are: " + JSON.stringify(newCredentials) + "\n");
+}
+changePassword("newPassword123");
